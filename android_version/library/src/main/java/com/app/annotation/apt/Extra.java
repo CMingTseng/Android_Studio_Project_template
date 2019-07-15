ff3b4f6a8f574441f2e0925ac685f52fafd3b02e
Neo <neo@cctips.com>
2019-07-16 02:00:00 +0800

    first commit

---
+++ android_version/library/src/main/java/com/app/annotation/apt/Extra.java
@@ -0,0 +1,16 @@
+package com.app.annotation.apt;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+/**
+ *
+ */
+@Retention(CLASS)
+@Target(FIELD)
+public @interface Extra {
+    String value();
+}
